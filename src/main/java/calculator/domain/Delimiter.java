a3d056bfafb6563c6d909641665d327f1f74baa0
Jaeminjeong1 <82368069+Jaeminjeong1@users.noreply.github.com>
2025-10-14 19:12:39 +0900

    feat: input을 split하는 메소드 구현

--- src/main/java/calculator/domain/Delimiter.java
+++ src/main/java/calculator/domain/Delimiter.java
@@ -25,6 +25,21 @@ public class Delimiter {
         return null;
     }
 
+    public List<String> splitString(String userInput, List<String> delimiters) {
+        List<String> strings;
+        String splitFormatString = splitFormat(delimiters);
+
+        //커스텀 구분자 있을떄
+        if (userInput.startsWith(START_CUSTOM_DELIMITER)) {
+            String afterUserInput = userInput.substring(userInput.lastIndexOf(END_CUSTOM_DELIMITER) + 2);
+            strings = List.of(afterUserInput.split(splitFormatString));
+            return strings;
+        }
+        //기본 구분자만 있을때
+        strings = List.of(userInput.split(splitFormatString));
+        return strings;
+    }
+
     private String splitFormat(List<String> delimiters) {
         StringBuilder sb = new StringBuilder();
 
