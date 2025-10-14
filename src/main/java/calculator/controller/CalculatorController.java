a8d0e6dcc2c1b4eb4564dff35677348daffa80e5
Jaeminjeong1 <82368069+Jaeminjeong1@users.noreply.github.com>
2025-10-14 20:17:40 +0900

    feat: 결과 출력 기능 구현

--- src/main/java/calculator/controller/CalculatorController.java
+++ src/main/java/calculator/controller/CalculatorController.java
@@ -2,17 +2,20 @@ package calculator.controller;
 
 import calculator.service.CalculatorService;
 import calculator.view.InputView;
+import calculator.view.OutputView;
 
 import java.util.List;
 
 public class CalculatorController {
 
     private InputView inputView = new InputView();
+    private OutputView outputView = new OutputView();
     private CalculatorService calculatorService = new CalculatorService();
 
     public void start() {
         String userInput = inputView.getStartInput();
         List<String> strings = calculatorService.findNumbers(userInput);
         Object result = calculatorService.calculateResult(strings);
+        outputView.printResult(result);
     }
 }
--- src/main/java/calculator/view/OutputView.java
+++ src/main/java/calculator/view/OutputView.java
@@ -1,4 +1,10 @@
 package calculator.view;
 
 public class OutputView {
+
+    private static final String PRINT_RESULT = "결과 : ";
+
+    public void printResult(Object result) {
+        System.out.println(PRINT_RESULT + result);
+    }
 }
