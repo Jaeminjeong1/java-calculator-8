e3d6ca6d1bde5efac7c407c0a65411fbba14215e
Jaeminjeong1 <82368069+Jaeminjeong1@users.noreply.github.com>
2025-10-14 20:06:38 +0900

    feat: 계산 기능 구현

--- src/main/java/calculator/utils/Calculator.java
+++ src/main/java/calculator/utils/Calculator.java
@@ -1,4 +1,28 @@
 package calculator.utils;
 
+import java.util.List;
+
 public class Calculator {
+
+    private double result;
+
+    public double calculateNumber(List<String> numbers) {
+        for (String number : numbers) {
+            validateNum(number);
+            result += Double.parseDouble(number);
+        }
+        return result;
+    }
+
+    //숫자 검증
+    private void validateNum(String number) {
+        try {
+            double tempNum = Double.parseDouble(number);
+            if (tempNum <= 0) {
+                throw new IllegalArgumentException();
+            }
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException();
+        }
+    }
 }
